9119f475759a9dff403217f7cecf6773139c929a
Faimubarak <fainothing25@gmail.com>
2023-02-26 21:16:08 +0200

    add accounts

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from . import views
+from .views import profile , profile_edit , signup 
 
 app_name = 'accounts'
 
 urlpatterns = [
-
+    path('signup',signup , name='signup'),
+    path('profile/',profile,name='profile'),
+    path('profile/edit', profile_edit , name='profile_edit') ,
 ]
